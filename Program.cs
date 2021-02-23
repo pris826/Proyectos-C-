b02811fbb22c3347270822bb69f4da817a37949f
pris826 <priscijaz13@gmail.com>
2021-02-23 12:16:03 -0600

    hola mundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto_Hola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!");
+        }
+    }
+}
